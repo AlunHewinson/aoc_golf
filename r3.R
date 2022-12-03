8276248bbe2662b36b77d99b50ac004b72c6aa68
Alun Hewinson <alunhewinson@gmail.com>
2022-12-03 10:23:45 +0000

    Day 3 score improved using code from a fellow R4DS Slack user, Aron S

--- r3.R
+++ r3.R
@@ -1,8 +1,6 @@
-i=intersect
-m=\(m)i(m[,1],m[,2])
-z=\(q)match(q,c(letters,LETTERS))
-r=readLines
-p=strsplit
-x=matrix
-sum(sapply(p(r("3"),""),\(q)z(m(x(q,nc=2)))))
-sum(z(apply(x(r("3"),3),2,\(q){s=p(q,"");i(i(s[[1]],s[[2]]),s[[3]])})))
+R=\(q)Reduce(intersect,q)
+S=sapply
+L=length
+x=lapply(readLines("3"),\(y)(utf8ToInt(y)-38)%%58)
+sum(S(x,\(y)R(split(y,seq(y)<=L(y)/2))))
+sum(S(split(x,(seq(L(x))-1)%/%3),R))
