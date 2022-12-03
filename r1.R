dff6991c63f4878494ee0645d778f2fec8f49bd6
Alun Hewinson <alunhewinson@gmail.com>
2022-12-03 09:17:15 +0000

    reluctantly removing the print statements :(

--- r1.R
+++ r1.R
@@ -1,3 +1,3 @@
 i=sapply(gsub(" ","+",unlist(strsplit(paste(readLines("1"),collapse=" "),"  "))),\(q)eval(parse(text=q)))
-print(max(i))
-print(sum(rev(sort(i))[1:3]))
+max(i)
+sum(rev(sort(i))[1:3])
