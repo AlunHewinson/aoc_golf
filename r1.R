b394f00b4595f56c7011ff52e668496c2aa1e479
Alun Hewinson <alunhewinson@gmail.com>
2022-12-03 09:54:36 +0000

    make various improvements to the scores for days 1-3

--- r1.R
+++ r1.R
@@ -1,3 +1,3 @@
-i=sapply(gsub(" ","+",unlist(strsplit(paste(readLines("1"),collapse=" "),"  "))),\(q)eval(parse(text=q)))
+i=sapply(gsub(" ","+",unlist(strsplit(paste(readLines("1"),collapse=" "),"  "))),\(q)eval(parse(t=q)))
 max(i)
 sum(rev(sort(i))[1:3])
--- r3.R
+++ r3.R
@@ -1,5 +1,8 @@
 i=intersect
 m=\(m)i(m[,1],m[,2])
 z=\(q)match(q,c(letters,LETTERS))
-sum(sapply(strsplit(readLines("3"),""),\(q)z(m(matrix(q,nc=2)))))
-sum(z(apply(matrix(readLines("3"),3),2,\(q){s=strsplit(q,"");i(i(s[[1]],s[[2]]),s[[3]])})))
+r=readLines
+p=strsplit
+x=matrix
+sum(sapply(p(r("3"),""),\(q)z(m(x(q,nc=2)))))
+sum(z(apply(x(r("3"),3),2,\(q){s=p(q,"");i(i(s[[1]],s[[2]]),s[[3]])})))
